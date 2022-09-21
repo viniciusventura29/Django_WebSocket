4a6de79a6d708ac935d37d434985201361238cae
viniciusventura29 <viniciusventura29@gmail.com>
2022-09-21 11:00:02 -0300

    starting to do auth part

--- home/consumers.py
+++ home/consumers.py
@@ -40,20 +40,26 @@ class WSConsumer(AsyncWebsocketConsumer):
     async def receive(self,text_data):
         text_data_json = json.loads(text_data);
         message = text_data_json['message'];
+        username = text_data_json['username'];
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message':message,
+                'username':username,
+
             }
         )
     
     async def chat_message(self, event):
         message = event['message']
+        username = event['username']
+
 
         await self.send(text_data = json.dumps({
             'message':message,
+            'username':username,
         }))
 
         
